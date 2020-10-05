a1e0872ef54c454ab0dfabdb0265ec7595656b51
Greg Roques <greg.roques@gmail.com>
2020-10-05 08:28:28 -0400

    building out backend and redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignUp();
+  }
+
   NoPage = () => {
     return <Redirect push to="/" />;
   };
 
   render() {
-    const TESTisLoggedIn = true;
+    const { isLoggedIn } = this.props;
 
     return (
       <div>
@@ -22,4 +27,10 @@ class App extends Component {
   }
 }
 
-export default withRouter(App);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignUp: () => dispatch(authCheckState()),
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
