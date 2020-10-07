a4fa2f56655fdab87fba3c6297ab401d35a37324
Greg Roques <greg.roques@gmail.com>
2020-10-07 11:16:43 -0400

    update

--- src/Dependencies/userInfo.js
+++ src/Dependencies/userInfo.js
@@ -1,15 +1,16 @@
 import axios from "axios";
-import { browserName, osName, osVersion } from "react-device-detect";
+import { browserName } from "react-device-detect";
 // browserVersion -- gets latest version of browser installed from react-device-detect
+// osName, osVersion -- if you include the operating system and its versin used by user from react-device-detect
 
 let ipAddress;
-let location;
+// let location;
 
 axios("https://extreme-ip-lookup.com/json/")
   .then((res) => {
     const { query, countryCode, city, region } = res.data;
     ipAddress = query;
-    location = `${city}, ${region}, ${countryCode}`;
+    // location = `${city}, ${region} - ${countryCode}`;
   })
   .catch(() => {
     console.log("Request failed");
@@ -17,7 +18,7 @@ axios("https://extreme-ip-lookup.com/json/")
 
 export const compId = {
   browserType: browserName,
+  // osType: `${osName}: ${osVersion}`,
   ip: ipAddress,
-  location: location,
+  // location: location,
 };
-// osType =  `${osName}: ${osVersion}`, -- if you include the operating system and its versin used by user from react-device-detect
