accff4db68db53dbf1e085c298f95ba0dae4a043
Greg Roques <greg.roques@gmail.com>
2020-10-10 18:41:24 -0400

    bcrypt and rand token

--- back-end/routes/logIn.js
+++ back-end/routes/logIn.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const { databasePassword, TEST_isLoggedIn } = require("../util/password");
+const { signInPW, TEST_isLoggedIn } = require("../util/password");
 
 router.post("/", (req, res, next) => {
   const { password, compId } = req.body;
 
-  bcrypt.compare(password, databasePassword, function (err, result) {
-    if (password === result) {
+  bcrypt.compare(password, signInPW, function (err, result) {
+    if (result) {
       res.json(TEST_isLoggedIn);
       console.log(compId);
     } else {
