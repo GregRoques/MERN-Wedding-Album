38f75e74a732c08afe9a445cf22da3dbb7a49fc7
Greg Roques <greg.roques@gmail.com>
2020-12-27 12:20:17 -0500

    udpate

--- back-end/routes/video.js
+++ back-end/routes/video.js
@@ -1,30 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const axios = require("axios");
+const { ytKey } = require("../util/passwords/ytKey");
 const { isAuthenticated } = require("../util/middleware/authenticator");
 
-const videoList = [
-  {
-    title: 'Feature',
-    link: "https://www.youtube.com/embed/Sq8uC9pEd_0"
-  },
-  {
-    title: 'Full',
-    link: "https://www.youtube.com/embed/wmxQ7R4ZYgE"
-  },
-  {
-    title: 'Chris Stuckey Live 1',
-    link: "https://www.youtube.com/embed/sI14foi9ySg"
-  },
-  {
-    title: 'Chris Stuckey Live 2',
-    link: "https://www.youtube.com/embed/mTJ5xb-Bkpo"
-  }
-];
+let videoList = {
+  videoLinks: [],
+  playlistLinkId: "PLM2GdNHvfSCElP-c0kHDV6nYszw1mxfkm",
+};
+
+const getVids = () => {
+  videoList.videoLinks = [];
+
+  axios("https://www.googleapis.com/youtube/v3/playlistItems", {
+    params: {
+      key: ytKey,
+      channelId: "UCnPRjTTsNcJC0VYo2fQ1ZPw",
+      playlistId: videoList.playlistLinkId,
+      part: "snippet, id, contentDetails",
+    },
+  })
+    .then((res) => {
+      //console.log(res.data.items);
+      const videoFeedback = res.data.items;
+      videoFeedback.map((vid) => {
+        videoList.videoLinks.push({
+          title: vid.snippet.title,
+          link: `https://www.youtube.com/embed/${vid.contentDetails.videoId}`,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      console.log(`ERROR RETURNING YOUTUTBE VIDEOS FROM GOOGLE-API: ${err}`);
+    });
+};
+
+getVids();
+
+setInterval(() => {
+  getVids();
+}, 90000000); // updates every 25 hours...one hour apart from photo check;
 
 router.post("/", isAuthenticated, (req, res, next) => {
+  console.log(videoList)
   res.json(videoList);
 });
 
 module.exports = router;
-
-
