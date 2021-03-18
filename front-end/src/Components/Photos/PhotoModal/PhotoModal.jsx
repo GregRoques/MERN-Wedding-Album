c0cc5e2090011a9f11147723c6dbe5f469913c1b
Greg Roques <42392687+GregRoques@users.noreply.github.com>
2021-03-18 17:02:50 -0400

    Update PhotoModal.jsx

--- front-end/src/Components/Photos/PhotoModal/PhotoModal.jsx
+++ front-end/src/Components/Photos/PhotoModal/PhotoModal.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import cssPhotoModal from './photoModal.module.css'
+import {css} from 'emotion';
 
 const PhotoModal = props => {
-    const { image, imageIndex, nextDirection, closeModal, isShown, stopAutoDownload, totalLength } = props;
+        const { image, imageIndex, nextDirection, closeModal, isShown, stopAutoDownload, totalLength, degrees, rotate } = props;
+        const imgStyle = css`
+            transform: scale(${degrees === 90 || degrees === 270 ? '0.75' : '1'}) rotate(${degrees}deg);
+        `;
     return  isShown ? (
         <div className= { cssPhotoModal.photoModal } >
         <div className={ cssPhotoModal.closePhotoModal } onClick={()=> closeModal(false, null)}>x</div>
@@ -14,7 +18,7 @@ const PhotoModal = props => {
             }   
             </div>
             <div className={ cssPhotoModal.sliderContainer } onDragStart={e=> stopAutoDownload(e)} onContextMenu={e=> stopAutoDownload(e)}>
-                <img name={image} alt={image} src={`/images/weddingAlbum/web/med_${image}`}/>
+                <img className={imgStyle} name={image} alt={image} src={`/images/weddingAlbum/web/med_${image}`}/>
             </div>
             <div className={ cssPhotoModal.imageGalleryButtons }>
                 { imageIndex !== totalLength ?
@@ -26,8 +30,11 @@ const PhotoModal = props => {
         <div className ={ cssPhotoModal.pictureCounter }>
             <a href={`/images/weddingAlbum/full/${image}`} download={image}> <img src='/images/download.png'/></a>
         </div>
+        <div className ={ cssPhotoModal.rotate } onClick={()=>rotate()}>
+
+                R
+        </div>
     </div>
-    ) : ""
-}
+    ) : ""}
 
 export default PhotoModal;
